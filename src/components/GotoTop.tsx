d1534030445a5d3a92aaa7e30ee7af2922c7f9a0
sanyachokedee <ing11819@gmail.com>
2025-02-09 13:30:40 +0700

    v4

--- src/components/GotoTop.tsx
+++ src/components/GotoTop.tsx
@@ -1,9 +1,7 @@
-
-export  function GotoTop() {
-
-    const scrollToTop = () => {
+const scrollToTop = () => {
       window.scrollTo({ top: 0, left: 0, behavior: 'smooth' });
     };
+export  function GotoTop() {    
     return (
     <div className="flex items-center justify-center py-2">
         <button onClick={scrollToTop} className="text-white bg-amber-300 text-center p-2 rounded w-1/4">Go to top</button>
